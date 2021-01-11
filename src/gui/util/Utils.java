ee6333c9d606c266845d7b312e4502d2dc4636d6
Ravel Andrade <ravel.gou@gmail.com>
2021-01-11 18:18:01 -0300

    Productform

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,32 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	
+}
